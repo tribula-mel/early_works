dec36c9dc74f32243aa9cf5df9b621594a938227
Boban Petrovic <boban.petrovic@gmail.com>
2026-05-23 12:18:55 +0200

    ai calculator design

--- dudson_early_stuff/song.py
+++ dudson_early_stuff/song.py
@@ -1,10 +1,11 @@
 import pygame
 import random
-import tkinter
+import tkinter as tk
 pygame.mixer.init()
 
 
 pygame.mixer.music.load("gm_amazing.mp3")
 pygame.mixer.music.play(loops = 0)
 
+root = tk.Tk()
 root.mainloop()
